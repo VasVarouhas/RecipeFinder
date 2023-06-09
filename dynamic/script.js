d20693ecd0ad7a82faef74ccdec45250fbeb1e29
VasVarouhas <vas_var@hotmail.gr>
2023-06-09 17:08:09 +0300

    Update script.js

--- dynamic/script.js
+++ dynamic/script.js
@@ -1,23 +1,37 @@
-// Select the form element and attach an event listener
-const searchForm = document.getElementById('search-form');
-searchForm.addEventListener('submit', handleSearch);
+document.getElementById('search-form').addEventListener('submit', async (e) => {
+  e.preventDefault(); // Prevent the default form submission behavior
 
-// Event handler function for the form submission
-function handleSearch(event) {
-  event.preventDefault(); // Prevent default form submission behavior
+  const searchQuery = document.getElementById('search-input').value;
 
-  // Retrieve the user's input from the search input field
-  const searchInput = document.getElementById('search-input').value;
+  // Make an AJAX request to the server's search endpoint
+  const response = await fetch(`https://season-kind-canvas.glitch.me/server.js/api/search?query=${encodeURIComponent(searchQuery)}`);
+  const data = await response.json();
 
-  // Perform the necessary actions, such as making API calls or processing the search query
-  // You can call a separate function here to handle the search functionality
-  performSearch(searchInput);
+  // Update the UI with the search results
+  displaySearchResults(data.results);
+});
+
+function displaySearchResults(results) {
+  const recipeCardsContainer = document.getElementById('recipeCards');
+  recipeCardsContainer.innerHTML = ''; // Clear the existing recipe cards
+
+  // Create recipe cards based on the results
+  results.forEach((recipe) => {
+    const card = document.createElement('div');
+    card.className = 'recipeCard';
+
+    // Create the card's HTML structure based on your desired layout
+    // Use recipe data to fill in the card's information
+    // Example:
+    card.innerHTML = `
+      <img src="${recipe.image}" alt="Recipe Image">
+      <h2 class="recipe-title">${recipe.title}</h2>
+      <ul class="recipe-ingredients">
+        ${recipe.ingredients.map((ingredient) => `<li>${ingredient}</li>`).join('')}
+      </ul>
+      <p class="recipe-instructions">${recipe.instructions}</p>
+    `;
+
+    recipeCardsContainer.appendChild(card);
+  });
 }
-function performSearch(query) {
-    // Make API calls or perform necessary actions to fetch recipes based on the search query
-  
-    // Example: Log the search query in the console
-    console.log('Search Query:', query);
-    
-    // You can continue implementing the logic here, such as making API requests and updating the UI with search results
-  }
