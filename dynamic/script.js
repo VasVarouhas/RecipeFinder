d1710e11c5fe19e58cec93fd31c15b097838ab56
VasVarouhas <vas_var@hotmail.gr>
2023-06-11 19:01:46 +0300

    update

--- dynamic/script.js
+++ dynamic/script.js
@@ -1,76 +1,80 @@
-document.getElementById('search-form').addEventListener('submit', async (e) => {
-  e.preventDefault(); // Prevent the default form submission behavior
+document.addEventListener('DOMContentLoaded', () => {
+  const searchForm = document.getElementById('search-form');
+  const searchInput = document.getElementById('search-input');
+  const recipeCardsContainer = document.getElementById('recipeCards');
 
-  const searchQuery = document.getElementById('search-input').value;
+  searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault(); // Prevent the form from submitting normally
 
-  try {
-    // Make an AJAX request to the server's search endpoint
-    const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${encodeURIComponent(searchQuery)}`);
-    
-    if (!response.ok) {
-      throw new Error('Request failed');
+    const query = searchInput.value.trim(); // Get the search query from the input field
+
+    if (query === '') {
+      // If the query is empty, clear the recipe cards container
+      recipeCardsContainer.innerHTML = '';
+      return;
     }
-    
-    const data = await response.json();
 
-    console.log(data); // Output the entire response data object
-    
-    // Check the structure of the data and log specific properties for verification
-    data.results.forEach((recipe) => {
-      console.log(recipe.title); // Output the title of each recipe
+    try {
+      // Make a GET request to your server's recipe search endpoint
+      const response = await axios.get(`/api/search?query=${query}`);
+
+      const { results } = response.data;
+
+      // Clear the recipe cards container
+      recipeCardsContainer.innerHTML = '';
+
+      if (results.length === 0) {
+        // If no recipes were found, display a message
+        recipeCardsContainer.innerHTML = '<p>No recipes found</p>';
+      } else {
+        // Loop through the recipe results and create recipe cards
+        results.forEach((recipe) => {
+          const recipeCard = createRecipeCard(recipe);
+          recipeCardsContainer.appendChild(recipeCard);
+        });
+      }
+    } catch (error) {
+      console.error('Error fetching recipes:', error);
+
+      if (error.response) {
+        console.error('Response data:', error.response.data);
+      }
+
+      // Display an error message
+      recipeCardsContainer.innerHTML = '<p>Error fetching recipes</p>';
+    }
+  });
+
+  function createRecipeCard(recipe) {
+    // Create elements for the recipe card
+    const card = document.createElement('div');
+    card.classList.add('recipe-card');
+
+    const image = document.createElement('img');
+    image.src = recipe.image;
+    image.alt = recipe.title;
+    card.appendChild(image);
+
+    const title = document.createElement('h2');
+    title.textContent = recipe.title;
+    card.appendChild(title);
+
+    const ingredients = document.createElement('ul');
+    recipe.ingredients.forEach((ingredient) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = ingredient;
+      ingredients.appendChild(listItem);
     });
-    
-    // Update the UI with the search results
-    displaySearchResults(data.results);
-  } catch (error) {
-    console.error(error);
-    // Handle error gracefully, e.g., display an error message to the user
+    card.appendChild(ingredients);
+
+    const instructionsLink = document.createElement('a');
+    instructionsLink.href = recipe.instructions;
+    instructionsLink.textContent = 'View Instructions';
+    card.appendChild(instructionsLink);
+
+    return card;
   }
 });
 
-function displaySearchResults(results) {
-  const recipeCardsContainer = document.getElementById('recipeCards');
-  recipeCardsContainer.innerHTML = ''; // Clear the existing recipe cards
-  // Check if results is an array
-  if (!Array.isArray(results)) {
-    console.error('Invalid results data:', results);
-    return; // Exit the function if results is not an array
-  }
-
-  // Create recipe cards based on the results
-  results.forEach((recipe) => {
-    const card = document.createElement('div');
-    card.className = 'recipeCard';
-
-    // Create the card's HTML structure based on your desired layout
-    // Use recipe data to fill in the card's information
-
-    const recipeImage = document.createElement('img');
-    recipeImage.src = recipe.image; // Use the actual image URL from the API response
-    recipeImage.alt = 'Recipe Image';
-    card.appendChild(recipeImage);
-
-    const recipeTitle = document.createElement('h2');
-    recipeTitle.className = 'recipe-title';
-    recipeTitle.textContent = recipe.title;
-    card.appendChild(recipeTitle);
-
-    const recipeIngredients = document.createElement('ul');
-    recipeIngredients.className = 'recipe-ingredients';
-    recipe.ingredients.forEach((ingredient) => {
-      const ingredientItem = document.createElement('li');
-      ingredientItem.textContent = ingredient;
-      recipeIngredients.appendChild(ingredientItem);
-    });
-    card.appendChild(recipeIngredients);
-
-    const recipeInstructions = document.createElement('p');
-    recipeInstructions.className = 'recipe-instructions';
-    recipeInstructions.textContent = recipe.instructions;
-    card.appendChild(recipeInstructions);
-
-    recipeCardsContainer.appendChild(card);
-  });
-}
 
 
